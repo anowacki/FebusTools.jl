a696b312c448e3d5ac78716dd7135a69e2ab729e
Andy Nowacki <a.nowacki@leeds.ac.uk>
2023-02-17 22:14:59 +0000

    Implement file reading
    - Add keyword argument constructor for `FebusData`.
    - Add distance cutting in `read_hdf5`
    - Add block-level cutting in `read_hdf5`

--- src/FebusTools.jl
+++ src/FebusTools.jl
@@ -1,6 +1,6 @@
 module FebusTools
 
-using Dates: Dates, DateTime, Second, Millisecond, datetime2unix, unix2datetime
+using Dates: Dates, DateTime, Second, Millisecond, Microsecond, datetime2unix, unix2datetime
 using HDF5: attrs, h5open
 using NanoDates: NanoDate
 using Statistics: mean
@@ -10,18 +10,38 @@ export
 
 
 struct FebusData{M,Da,Ti,Di}
+    "Matrix of strain or strain rate with dimensions (time, distance)"
     data::M
+    "Dates of each sample"
     dates::Da
+    "Times of each sample"
     times::Ti
+    "Distances of each channel"
     distances::Di
+    "Other information read from file"
     metadata::Dict{Symbol,Any}
 end
 
-"""
-    read_hdf5(file[, cut1[, cut2]]; zones, sources) -> ::FebusData
+# Keyword argument constructor
+FebusData(; data, dates, times, distances, metadata) =
+    FebusData(data, dates, times, distances, metadata)
 
-Read strain or strain rate data from `file` on disk, reading blocks between
-dates `cut1` and `cut2`.  By default, all data are read.
+"""
+    read_hdf5(file; xlim=(-Inf, Inf), tlim=($(typemin(DateTime)), $(typemax(DateTime))), blocks, zones=(>=(1)), sources=(>=(1))) -> ::FebusData
+
+Read strain or strain rate data from `file` on disk.  By default, all data are
+read into memory.
+
+If `xlim` is given a length-2 collection, then only channels whose distances
+lie between `xlim[begin]` and `xlim[end]` will be read.  Likewise, only blocks with
+times between `tlim[begin]` and `tlim[end]` are read.  Note that whole blocks
+(usuall of duration 1 s) are always read, so the stand and end times of the
+returned data can be respectively before and after `xlim[begin]` and `xlim[end]`.
+
+Instead of `tlim`, you can also choose which blocks are read in with the
+`blocks` keyword argument, which again takes two items, the start and end
+block index (which begins at 1).  Block indices outside the range of the data
+are warned about and only existing data which match the range are read.
 
 To limit data to a set of zones, supply a function of form `f(::Int)::Bool`
 to `zones` which returns `true` when its first argument is the number of the
@@ -34,9 +54,30 @@ source a set of source numbers.
     Currently only the first source of the first zone is read, and an error
     is thrown if more than one zone or source is present in a file.
 """
-function read_hdf5(file, cut1=typemin(DateTime), cut2=typemax(DateTime);
-    zones=(>=(1)), sources=(>=(1))
+function read_hdf5(file;
+    tlim::Union{Tuple,AbstractArray,Nothing}=nothing,
+    xlim::Union{Tuple,AbstractArray}=(-Inf, Inf),
+    blocks::Union{Tuple{Integer,Integer},AbstractArray{<:Integer},Nothing}=nothing,
+    zones=(>=(1)),
+    sources=(>=(1))
 )
+    # Requirement to pass two items and not a range or filtering function is
+    # because `HDF5` only slices with continuous ranges, which we can construct
+    # from length-2 collections.
+    if !isnothing(tlim) && length(tlim) != 2
+        throw(ArgumentError("`tlim` must contain two items"))
+    end
+    if length(xlim) != 2
+        throw(ArgumentError("`xlim` must contain two items"))
+    end
+
+    # Allow either time limiting by block indices or date, but not both
+    if !isnothing(tlim) && !isnothing(blocks)
+        throw(ArgumentError("only one of `tlim` or `blocks` can be used"))
+    elseif isnothing(tlim) && isnothing(blocks)
+        tlim = (typemin(DateTime), typemax(DateTime))
+    end
+
     metadata = Dict{Symbol,Any}()
 
     h5open(file, "r") do f
@@ -46,36 +87,52 @@ function read_hdf5(file, cut1=typemin(DateTime), cut2=typemax(DateTime);
         # Get source
         source_key = only(keys(sensor))
         source = sensor[source_key]
-        # Get times
-        block_times = source["time"][:]
-        block_dates = unix2datetime.(block_times)
+        # Timestamps of start of each block
+        block_start_times = source["time"][:]
+        block_start_dates = unix2datetime.(block_start_times)
         # Get zones
         zone_key = only(filter(!=("time"), keys(source)))
         zone = source[zone_key]
 
+        # Global attributes
+        # Total size of blocks before downsampling
+        whole_extent = metadata[:WholeExtent] = Int.(attrs(source)["WholeExtent"])
+
+        # Derived attributes using raw values
+        zone_atts = attrs(zone)
+        # Block rate mHz and in-block sample spacing (ms)
+        sampling_interval = zone_atts["Spacing"][2]/1000
+        sampling_rate = 1000/zone_atts["Spacing"][2]
+        sampling_interval_µs = Microsecond(zone_atts["BlockRate"][1]//Int(zone_atts["Spacing"][2]))
+
         # Get attributes for the first zone and convert to default types
-        for (k, v) in attrs(zone)
+        for (k, v) in zone_atts
             key = Symbol(k)
             val = length(v) == 1 ? only(v) : v
 
+            # Spatial sampling resolution
             if key == :SamplingRes
                 # Convert from cm -> m
                 val /= 100
+            # Rate of block writing
             elseif key == :BlockRate
                 metadata[:BlockLength] = 2000/val
+                metadata[:BlockInterval] = 1000/val
                 # mHz -> Hz
                 val /= 1000
+            # Frequency of laser pulses
             elseif key == :PulseRateFreq
                 # mHz -> Hz
                 val = Int(val)/1000
             elseif key == :DataDomain
                 val = Int.(val)
+            # Extent of blocks for this zone
             elseif key == :Extent
                 val = Int.(val)
+            # Raw sampling rate of instrument
             elseif key == :SamplingRate
-                metadata[:SamplingInterval] = Dates.Microsecond(1_000_000_000_000//val)
-                # µHz -> Hz
-                val /= 1_000_000
+                # mHz -> Hz
+                val /= 1_000
             elseif key == :DerivationTime
                 # ms -> s
                 val /= 1000
@@ -84,31 +141,90 @@ function read_hdf5(file, cut1=typemin(DateTime), cut2=typemax(DateTime);
             metadata[key] = val
         end
 
-        # Calculate recorded sampling interval in s
-        sampling_interval = metadata[:BlockLength]/metadata[:Extent][2]
-
         # Number of points per whole block
-        metadata[:nsamples_per_block] = metadata[:BlockLength]*metadata[:SamplingRate]
+        metadata[:nsamples_per_block] = whole_extent[4] - whole_extent[3] + 1
+        if metadata[:nsamples_per_block] != metadata[:BlockLength]*metadata[:PulseRateFreq]
+            @warn "Unexpected total block extent ($whole_extent) for block " *
+                "length ($(metadata[:BlockLength]) s) and pulse rate frequency " *
+                "($(metadata[:PulseRateFreq]) Hz)"
+        end
 
+        # Find distances along the fibre
         x0::Float64 = metadata[:Origin][1]
         distances = x0 .+ (metadata[:Extent][1]:metadata[:Extent][2]).*metadata[:Spacing][1]
-        # distances = 0:metadata[:Spacing][1]:metadata[:FiberLength]
 
-        ind1 = findfirst(x -> cut1 <= x <= cut2, block_dates)
-        ind2 = findlast(x -> cut1 <= x <= cut2, block_dates)
-        if isnothing(ind1)
-            @warn "No blocks between $cut1 and $cut2 in file '$file'"
+        # Calculate which blocks to include
+        total_nblocks = size(zone["StrainRate"], 3)
+
+        if !isnothing(tlim)
+            cut1, cut2 = tlim
+            t_ind1 = findfirst(x -> cut1 <= x <= cut2, block_start_dates)
+            t_ind2 = findlast(x -> cut1 <= x <= cut2, block_start_dates)
+            if isnothing(t_ind1)
+                @warn "No blocks between $cut1 and $cut2 in file '$file'"
+                return FebusData(Matrix{Float32}(undef, 0, 0), [], [], distances, metadata)
+            end
+        else
+            t_ind1, t_ind2 = blocks
+            if t_ind1 < 1
+                @warn "Start block $(t_ind1) is below data range (i.e., 1) in file '$file'"
+            end
+            if t_ind2 > total_nblocks
+                @warn "End block $(t_ind2) is after the last block ($(total_nblocks)) in file '$file'"
+            end
+        end
+
+        nblocks = t_ind2 - t_ind1 + 1
+
+        # Calculate which channels to include
+        x1, x2 = xlim
+        x_ind1 = findfirst(x -> x1 <= x <= x2, distances)
+        x_ind2 = findlast(x -> x1 <= x <= x2, distances)
+        if isnothing(x_ind1)
+            @warn "No channels between $x1 and $x2 m in file '$file'"
             return FebusData(Matrix{Float32}(undef, 0, 0), [], [], distances, metadata)
         end
-        nblocks = ind2 - ind1 + 1
-        data = let d = zone["StrainRate"]
-            permutedims(reshape(d[:,:,ind1:ind2], size(d, 1), :))
-        end
-        dates = block_dates[ind1] .+
-            (Millisecond(0):metadata[:SamplingInterval]:Millisecond(1000*nblocks))
-        times = (0:(size(data, 1) - 1)) .* sampling_interval
+        nchannels = length(x_ind1:x_ind2)
 
-        FebusData(data, dates, times, distances, metadata)
+        # Calculate which block sample numbers we need to avoid overlap (starting at 0)
+        block_num1, block_num2 = metadata[:Extent][3], metadata[:Extent][4]
+        ninds = block_num2 - block_num1 + 1
+        if ninds*sampling_interval < metadata[:BlockInterval]
+            error("block extent ($(block_num1:block_num2)) does not cover whole ",
+                "block spacing ($(metadata[:BlockInterval]) s).  Cannot read ",
+                "continuous data from file '$file'.")
+        end
+        block_num2 = block_num1 + round(Int, metadata[:BlockInterval]/sampling_interval) - 1
+
+        # Convert to Julia indices of HDF5 array block samples (starting at 1),
+        # always starting with the first available sample.
+        # TODO: Check if a later sample aligns better with UTC and use that?
+        block_ind1 = 1
+        block_ind2 = block_num2 - block_num1 + 1
+
+        nsamples = length(block_num1:block_num2)*nblocks
+
+        # Cut out data
+        data = let d = zone["StrainRate"]
+            permutedims(
+                reshape(
+                    d[x_ind1:x_ind2, block_ind1:block_ind2, t_ind1:t_ind2],
+                    nchannels,
+                    :
+                )
+            )
+        end
+
+        if size(data) != (nsamples, nchannels)
+            error("error in calculating data slice.  Expected shape ",
+                (nsamples, nchannels), "; got ", size(data))
+        end
+
+        block_dates = block_start_dates[t_ind1:t_ind2]
+        times = (block_num1 .+ (0:(nsamples - 1))) .* sampling_interval
+        cut_distances = distances[x_ind1:x_ind2]
+
+        FebusData(data, block_dates, times, cut_distances, metadata)
     end
 end
 
